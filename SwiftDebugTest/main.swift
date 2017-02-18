32ab7a40d1f3e853f7dc14617c4a3bd11218bfac
Razzile <comfycallum@gmail.com>
2017-02-18 15:30:52 +0000

    Initial Commit

---
+++ SwiftDebugTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftDebugTest
+//
+//  Created by callum taylor on 18/02/2017.
+//  Copyright © 2017 Satori. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
